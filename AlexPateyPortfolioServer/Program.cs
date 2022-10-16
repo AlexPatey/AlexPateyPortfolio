262c6782cc5133c0e73f78051d25e63a8a40a108
= <alex.patey@btinternet.com>
2022-10-16 13:33:43 +0100

    removed commented code that wasn't needed

--- AlexPateyPortfolioServer/Program.cs
+++ AlexPateyPortfolioServer/Program.cs
@@ -98,29 +98,3 @@ app.MapDelete("/delete-post-by-id/{postId}", async (int postId) =>
 }).WithTags("Posts Endpoints");
 
 app.Run();
-
-//var summaries = new[]
-//{
-//    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-//};
-
-//app.MapGet("/weatherforecast", () =>
-//{
-//    var forecast = Enumerable.Range(1, 5).Select(index =>
-//        new WeatherForecast
-//        (
-//            DateTime.Now.AddDays(index),
-//            Random.Shared.Next(-20, 55),
-//            summaries[Random.Shared.Next(summaries.Length)]
-//        ))
-//        .ToArray();
-//    return forecast;
-//})
-//.WithName("GetWeatherForecast");
-
-
-
-//internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
-//{
-//    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-//}
